7b52b5fedf6582e984e9e99548451d98207f580c
Doni Ramadhan <98083245+xsyn1100@users.noreply.github.com>
2022-04-09 22:10:52 +0700

    Update welcome.py

--- Rose/Inline/welcome.py
+++ Rose/Inline/welcome.py
@@ -50,7 +50,7 @@ async def commands_callbacc(_, CallbackQuery):
     await CallbackQuery.message.delete()
 
 tetz = """
-Rose which will ask new Group Members to verify them by solving an emoji | number captcha.
+Syn which will ask new Group Members to verify them by solving an emoji | number captcha.
 
 - /captcha - turn on captcha : There are two types of captcha
 - /remove - turn off captcha
